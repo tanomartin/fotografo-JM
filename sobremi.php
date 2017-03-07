de0cad115722f19d873731ec6f7b0496581c6909
tanomartin <tanomartin05@gmail.com>
2017-03-07 10:45:31 -0300

    redireccion sobre mi y contacto.

---
+++ sobremi.php
@@ -0,0 +1,18 @@
+<?php 
+	include('includes/templateEngine.inc.php');
+	include('backend/include/conexion.php');
+
+ 	$arrayAlbum = array();
+ 	$sqlAlbumes = "select * from albumes a where activo = 1";
+ 	$resAlbumes = $mysqli->query($sqlAlbumes);
+ 	$rows = $resAlbumes->num_rows;
+ 	if ($rows > 0) { 
+ 		$i=0;
+ 		while($albumMenu = $resAlbumes->fetch_assoc()) {
+ 			$arrayAlbum[$i] = $albumMenu;
+ 			$i++;
+ 		}
+ 	}
+	
+	$twig->display('sobremi.html', array("albumes" => $arrayAlbum));
+?>
