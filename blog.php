05fc1fb39a459b6f42934e3a12a347a478406091
Martin Mauro Zucchiatti <tanomartin05@gmail.com>
2017-03-11 19:22:03 -0300

    subida de blog

--- blog.php
+++ blog.php
@@ -4,13 +4,13 @@
 	include('includes/consultasNavbar.php');
 	
 	$arrayEntradas = array();
-	$sqlBlog = "select * from blog where activo = 1";
+	$sqlBlog = "select b.*, DATE_FORMAT(b.fecha,'%m-%d-%Y') as fecha from blog b where b.activo = 1 order by fecha DESC, id DESC";
 	$resBlog = $mysqli->query($sqlBlog);
 	$rows = $resBlog->num_rows;
 	if ($rows > 0) {
 		$e=0;
 		while($entradas = $resBlog->fetch_assoc()) {
-			$arrayEntradas[$i] = $entradas;
+			$arrayEntradas[$e] = $entradas;
 			$e++;
 		}
 	}
