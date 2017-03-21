b5b513a5330a8e78c69c3b37de40497c4933e6ff
Martin Mauro Zucchiatti <tanomartin05@gmail.com>
2017-03-21 20:04:11 -0300

    correccion de error de orden y subida de index

--- backend/album.editar.upfoto.php
+++ backend/album.editar.upfoto.php
@@ -9,13 +9,10 @@ try {
 	$idAlbum = $_GET['idAlbum'];
 	
 	$sqlDownFirst = "UPDATE fotos SET orden = -1 WHERE idAlbum = $idAlbum and orden = $ordenAnterior";
-	echo $sqlDownFirst."<br>";
 	mysqli_query($mysqli, $sqlDownFirst);
 	$sqlUpFoto = "UPDATE fotos SET orden = $ordenAnterior WHERE idAlbum = $idAlbum and orden = $orden";
-	echo $sqlUpFoto."<br>";
 	mysqli_query($mysqli, $sqlUpFoto);
 	$sqlDownLast = "UPDATE fotos SET orden = $orden WHERE idAlbum = $idAlbum and orden = -1";
-	echo $sqlDownLast."<br>";
 	mysqli_query($mysqli, $sqlDownLast);
 
 	mysqli_commit($mysqli);
