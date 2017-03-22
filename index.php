e104f3cf9b4df9c69ff894ef6191e714af42bd66
tanomartin <tanomartin05@gmail.com>
2017-03-22 11:57:52 -0300

    primera aproximacion de video y incluimos el footer como hacemos con el navbar

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 	include('includes/templateEngine.inc.php');
-	$videoPath = "fotos/default/";
+	
 	function detect() {
-		$browser=array("IE","OPERA","MOZILLA","NETSCAPE","FIREFOX","SAFARI","CHROME");
+		$browser=array("SAFARI","CHROME","IE","OPR","MOZILLA","NETSCAPE","FIREFOX","TRIDENT");
 		$os=array("WIN","MAC","LINUX");
 	
 		# definimos unos valores por defecto para el navegador y el sistema operativo
@@ -10,22 +10,20 @@
 		$info['os'] = "OTHER";
 	
 		# buscamos el navegador con su sistema operativo
-		foreach($browser as $parent)
-		{
+		foreach($browser as $parent) {
 			$s = strpos(strtoupper($_SERVER['HTTP_USER_AGENT']), $parent);
 			$f = $s + strlen($parent);
 			$version = substr($_SERVER['HTTP_USER_AGENT'], $f, 15);
 			$version = preg_replace('/[^0-9,.]/','',$version);
-			if ($s)
-			{
+			
+			if ($s) {
 				$info['browser'] = $parent;
 				$info['version'] = $version;
 			}
 		}
 	
 		# obtenemos el sistema operativo
-		foreach($os as $val)
-		{
+		foreach($os as $val) {
 			if (strpos(strtoupper($_SERVER['HTTP_USER_AGENT']),$val)!==false)
 				$info['os'] = $val;
 		}
@@ -35,22 +33,22 @@
 	}
 	
 	$info=detect();
-	$browser = $info["browser"];
 	
-	if(strpos($browser, 'MSIE') !== FALSE || strpos($browser, 'Edge') !== FALSE || strpos($browser, 'Trident') !== FALSE) {
-		$videoPath = $videoPath."video-home.wmv";
+	$videoPath = "fotos/default/";
+	$browser = $info["browser"]; 
+	if(strpos($browser, 'IE') !== FALSE ) {
+		$videoPath .= "video-home.wmv";
 	}
-	if(strpos($browser, 'Opera Mini') !== FALSE || strpos($browser, 'Opera') !== FALSE || strpos($browser, 'OPR') !== FALSE) {
-		$videoPath = $videoPath."video-home.ogg";
+	if(strpos($browser, 'SAFARI') !== FALSE) {
+		$videoPath .= "video-home.ogg";
 	}
-	if(strpos($browser, 'Firefox') !== FALSE || strpos($browser, 'Chrome') !== FALSE || strpos($browser, 'Safari') !== FALSE) {
-		$videoPath = $videoPath."video-home.mp4";
+	if(strpos($browser, 'OPR') !== FALSE || strpos($browser, 'TRIDENT') !== FALSE || strpos($browser, 'FIREFOX') !== FALSE || strpos($browser, 'CHROME') !== FALSE || strpos($browser, 'FIREFOX') !== FALSE) {
+		$videoPath .= "video-home.mp4";
 	}
 	if ($videoPath == "fotos/default/") {
 		header('Location: home.php');	
 	} else {
 		$twig->display('index.html', array("videoPath" => $videoPath));
 	}
-	echo $videoPath."<br>";
-	echo $browser;
+	
 ?>
