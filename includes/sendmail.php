01e1cf70f5da8ce7ca87dafaf401b850246030ad
tanomartin <tanomartin05@gmail.com>
2017-03-23 16:12:10 -0300

    formulario de contacto andando

--- includes/sendmail.php
+++ includes/sendmail.php
@@ -1,29 +1,22 @@
 <?php
-// Guardar los datos recibidos en variables:
+
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
 $telefono = $_POST['telefono'];
 $mensaje = $_POST['mensaje'];
-// Definir el correo de destino:
-$dest = "tanomartin05@gmail.com"; 
- 
-// Estas son cabeceras que se usan para evitar que el correo llegue a SPAM:
-$headers = "From: $nombre <$email>\r\n";  
-$headers .= "X-Mailer: PHP5\n";
-$headers .= 'MIME-Version: 1.0' . "\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
- 
-// Aqui definimos el asunto y armamos el cuerpo del mensaje
-$asunto = "Contacto";
-$cuerpo = "Nombre: ".$nombre."<br>";
-$cuerpo .= "Email: ".$email."<br>";
-$cuerpo .= "Telefono: ".$telefono."<br>";
-$cuerpo .= "Mensaje: ".$mensaje;
- 
-// Esta es una pequena validación, que solo envie el correo si todas las variables tiene algo de contenido:
-if($nombre != '' && $email != '' && $telefono != '' && $mensaje != '') {
-   echo mail($dest,$asunto,$cuerpo,$headers);
+
+$to = "contacto@josemoroni.com.ar";
+$subject = "Mensaje a traves del formulario de contacto";
+$contenido = "Nombre: $nombre\n\n";
+$contenido .= "Email: $email\n\n";
+$contenido .= "Telefono: $telefono\n\n";
+$contenido .= "Comentario: $mensaje\n\n";
+$header = "From: Formulario de contacto <no-reply@c0300434.ferozo.com>\nReply-To: $email\n";
+$header .= "Mime-Version: 1.0\n";
+$header .= "Content-Type: text/plain";
+if(mail($to, $subject, $contenido ,$header)){
+	echo 1;
 } else {
-   echo false;
+	echo 0;
 }
 ?>
